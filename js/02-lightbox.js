e7fa4505917d4ff1d1684e11e0c80020ba7d6e8f
Midland707 <rus14job@ukr.net>
2022-12-11 22:04:54 +0200

    mission complete: task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -11,9 +11,9 @@
 // Виконуй це завдання у файлах 02-lightbox.html і 02-lightbox.js. Розбий його на декілька підзавдань:
 
 // Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// икористовуй готовий код з першого завдання.
+// використовуй готовий код з першого завдання.
 // Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs.
-// Необхідно додати посилання на два файли: simple - lightbox.min.js і simple - lightbox.min.css.
+// Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
 // Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
 // Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
 // Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt.
@@ -23,3 +23,22 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const items = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`,
+  ""
+);
+galleryList.insertAdjacentHTML("beforeend", items);
+
+// open modal with large image if klick to small image
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
